94f4b80f8e4e5b8af2f5082ce0668f9cd0b1ef5e
Todd Persen <todd.persen@gmail.com>
2012-06-23 02:58:23 -0400

    Clean up the Rails initializer generator.

--- lib/rails/generators/errplane/errplane_generator.rb
+++ lib/rails/generators/errplane/errplane_generator.rb
@@ -5,14 +5,14 @@ class ErrplaneGenerator < Rails::Generators::Base
 
   source_root File.expand_path('../templates', __FILE__)
   argument :api_key,
-    required: false,
+    required: true,
     type: :string,
-    description: "API Key for your Errplane Organization"
+    description: "API key for your Errplane organization"
   argument :application_id,
     required: false,
-    default: lambda { Time.now.to_s },
+    default: SecureRandom.hex(4),
     type: :string,
-    description: "API Key for your Errplane Organization"
+    description: "Identifier for this application (Leave blank and a new one will be generated for you)"
 
   def copy_initializer_file
     template "initializer.rb", "config/initializers/errplane.rb"
