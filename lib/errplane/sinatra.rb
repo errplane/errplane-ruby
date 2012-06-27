573fc007f60e425b2a6235874210175800eb45ce
Todd Persen <todd.persen@gmail.com>
2012-06-27 01:43:42 -0400

    Grabbing more data from Sinatra.

--- lib/errplane/sinatra.rb
+++ lib/errplane/sinatra.rb
@@ -1,10 +1,13 @@
-# Errplane.configure(true) do |config|
-  # config.logger            ||= ::Sinatra.logger
-  # config.rails_environment ||= ::Sinatra.environment
-  # config.rails_root        ||= ::Sinatra.root
-  # config.framework           = "Sinatra"
-  # config.framework_version   = ::Sinatra::VERSION
-# end
+Errplane.configure(true) do |config|
+  config.logger                ||= logger
+  config.rails_environment     ||= settings.environment
+  config.rails_root            ||= settings.root
+  config.framework               = "Sinatra"
+  config.framework_version       = ::Sinatra::VERSION
+  config.language                = "Ruby"
+  config.language_version        = "#{RUBY_VERSION}-p#{RUBY_PATCHLEVEL}"
+  config.environment_variables   = ENV.to_hash
+end
 
 error do
   Errplane.transmit_unless_ignorable(request.env['sinatra.error'], request.env)
--- lib/errplane/version.rb
+++ lib/errplane/version.rb
@@ -1,3 +1,3 @@
 module Errplane
-  VERSION = "0.1.7"
+  VERSION = "0.1.8"
 end
