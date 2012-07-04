d498b87765094b154ae4b0586bbff74646b31359
Todd Persen <todd.persen@gmail.com>
2012-07-04 13:47:12 -0400

    Clean up capistrano hook.

--- lib/errplane/capistrano.rb
+++ lib/errplane/capistrano.rb
@@ -7,9 +7,15 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :deploy do
     desc 'Notify Errplane of the deployment'
     task :notify_errplane, :except => {:no_release => true} do
+      set(:deploying_user) { `whoami`.strip }
+      set(:deploying_user_name) { `bash -c 'git config --get user.name'`.strip }
+      set(:deploying_user_email) { `bash -c 'git config --get user.email'`.strip }
+
       puts "Notifying Errplane of the deployment.."
       framework_env = fetch(:rails_env, fetch(:errplane_env, 'production'))
       load File.join(Dir.pwd, "config/initializers/errplane.rb")
+
+      Errplane.configuration.logger = Logger.new("/dev/null")
       Errplane.configuration.rails_environment = framework_env
 
       deploy_options = {
@@ -17,8 +23,13 @@ Capistrano::Configuration.instance(:must_exist).load do
         :revision => current_revision,
         :repository => repository,
         :scm => scm,
-        :host => host
+        :host => host,
+        :remote_user => (user rescue nil),
+        :local_user => deploying_user,
+        :local_user_name => deploying_user_name,
+        :local_user_email => deploying_user_email
       }
+
       Errplane::Transmitter.new.relay(deploy_options, true)
       puts 'Done.'
     end
