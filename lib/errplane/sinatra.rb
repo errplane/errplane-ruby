f60f0a22747ebbca4a101fe60e4830a0bd9240e1
Matthew Kanwisher <matt@kanwisher.com>
2012-08-07 11:52:48 -0400

    adding guards for modular sinatra

--- lib/errplane/sinatra.rb
+++ lib/errplane/sinatra.rb
@@ -1,12 +1,14 @@
 Errplane.configure(true) do |config|
   config.logger                ||= ENV['rack.logger']
-  config.rails_environment     ||= settings.environment
-  config.application_root            ||= settings.root
+  config.rails_environment     ||= settings.environment if defined?(settings)
+  config.application_root            ||= settings.root if defined?(settings)
   config.framework               = "Sinatra"
   config.framework_version       = ::Sinatra::VERSION
 end
 
-error do
-  Errplane.transmit_unless_ignorable(request.env['sinatra.error'], request.env)
-  raise request.env['sinatra.error']
+if defined?(error)
+	error do
+	  Errplane.transmit_unless_ignorable(request.env['sinatra.error'], request.env)
+	  raise request.env['sinatra.error']
+	end
 end
