8fc26604d3242870ea2a6b22d884cf00ac12d284
Todd Persen <todd.persen@gmail.com>
2012-09-06 11:29:17 -0400

    Remove extraneous logging from HijackRenderException.

--- lib/errplane/rails/middleware/hijack_render_exception.rb
+++ lib/errplane/rails/middleware/hijack_render_exception.rb
@@ -8,9 +8,7 @@ module Errplane
 
         def render_exception_with_errplane(env, e)
           controller = env["action_controller.instance"]
-          Errplane.configuration.logger.info("Controller: #{controller}")
           request_data = controller.try(:errplane_request_data) || {}
-          Errplane.configuration.logger.info("Request Data: #{request_data}")
           unless Errplane.configuration.ignore_user_agent?(request_data[:user_agent])
             Errplane.transmit_unless_ignorable(e, request_data)
           end
--- lib/errplane/version.rb
+++ lib/errplane/version.rb
@@ -1,3 +1,3 @@
 module Errplane
-  VERSION = "0.4.10"
+  VERSION = "0.4.12"
 end
