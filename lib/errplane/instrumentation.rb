4b9810702e6b22c60849b3b1898e6e0cae3d3301
Todd Persen <todd.persen@gmail.com>
2012-10-02 02:23:38 -0400

    Get gem booting with instrumentation.

--- lib/errplane/instrumentation.rb
+++ lib/errplane/instrumentation.rb
@@ -16,20 +16,58 @@ module Errplane
     end
   end
 
-  PLOGGER = [] #Logger.new("log/perfs.log", shift_age = 7, shift_size = 5048576)
+  class Instrumentation
+    class << self
+      def post_data(data)
+        log :info, "Posting data!\n -#{data}"
 
-  def post_data(data)
-    PLOGGER << "Posting data!\n -#{data}"
+        app_key = "app4you2love"
+        env_key = "production"
+        log :info, "<<<\n"
+        log :info, data
+        log :info, "<<<\n"
 
-    app_key = "app4you2love"
-    env_key = "production"
-    PLOGGER << "<<<\n"
-    PLOGGER << data
-    PLOGGER << "<<<\n"
+        http = Net::HTTP.new("api1.errplane.com", "8086")
+        response = http.post("/api/v2/time_series/applications/#{app_key}/environments/#{env_key}?api_key=ignored", data)
+        log :info, "-response.code #{response.code}-\n"
+      end
 
-    http = Net::HTTP.new("api1.errplane.com", "8086")
-    response = http.post("/api/v2/time_series/applications/#{app_key}/environments/#{env_key}?api_key=ignored", data)
-    PLOGGER << "-response.code #{response.code}-\n"
+      def spawn_thread()
+        Thread.new do
+          while true
+            sleep(2) # Take a little break, so an idle running worker doesn't keep the system busy
+            begin
+              # PLOGGER << "Waking up !-#{ErrplaneNotifications.notifications.inspect}"
+              out = []
+              while !ErrplaneNotifications.notifications.empty?
+                log :info, "not empty!"
+                n = ErrplaneNotifications.notifications.pop
+                timediff = n[:finish] - n[:start]
+                log :info, "------------------------\n"
+                s = ["notification2:", n[:name], timediff, n[:nid], n[:payload], "\n"].join("\t")
+                log :info, s
+                if( n[:name].to_s == "process_action.action_controller" )
+                  out << "controllers/#{n[:payload][:controller]}/actions/#{n[:payload][:action]} #{(timediff*1000).ceil} #{n[:finish].utc.to_i}"
+                  out << "controllers/#{n[:payload][:controller]}/actions/#{n[:payload][:action]}/views #{n[:payload][:view_runtime].ceil} #{n[:finish].utc.to_i }"
+                  out << "controllers/#{n[:payload][:controller]}/actions/#{n[:payload][:action]}/db #{n[:payload][:db_runtime].ceil} #{n[:finish].utc.to_i }"
+                  out << "controllers #{(timediff*1000).ceil} #{n[:finish].utc.to_i }"
+                  out << "views #{n[:payload][:view_runtime].ceil} #{n[:finish].utc.to_i }"
+                  out << "db #{n[:payload][:db_runtime].ceil} #{n[:finish].utc.to_i }"
+                  post_data(out.join("\n"))
+                else
+                  log :info, "name-#{n[:name]}-"
+                end
+                log :info, "Popped!"
+              end
+              #POST HERE
+              #post_data(out.join("\n"))
+            rescue => e
+              log :info, "Error-#{e.inspect}\n"
+            end
+          end
+        end
+      end
+    end
   end
 
   ActiveSupport::Notifications.subscribe do |name, start, finish, id, payload|
@@ -41,51 +79,15 @@ module Errplane
   #  Rails.logger.error("Pushed!-#{ErrplaneNotifications.notifications.inspect}")
   end
 
-  def spawn_thread()
-    Thread.new do
-      while true
-        sleep(2) # Take a little break, so an idle running worker doesn't keep the system busy
-        begin
-          # PLOGGER << "Waking up !-#{ErrplaneNotifications.notifications.inspect}"
-          out = []
-          while !ErrplaneNotifications.notifications.empty?
-            PLOGGER << "not empty!"
-            n = ErrplaneNotifications.notifications.pop
-            timediff = n[:finish] - n[:start]
-            PLOGGER <<  "------------------------\n"
-            s = ["notification2:", n[:name], timediff, n[:nid], n[:payload], "\n"].join("\t")
-            PLOGGER << s
-            if( n[:name].to_s == "process_action.action_controller" )
-              out << "controllers/#{n[:payload][:controller]}/actions/#{n[:payload][:action]} #{(timediff*1000).ceil} #{n[:finish].utc.to_i}"
-              out << "controllers/#{n[:payload][:controller]}/actions/#{n[:payload][:action]}/views #{n[:payload][:view_runtime].ceil} #{n[:finish].utc.to_i }"
-              out << "controllers/#{n[:payload][:controller]}/actions/#{n[:payload][:action]}/db #{n[:payload][:db_runtime].ceil} #{n[:finish].utc.to_i }"
-              out << "controllers #{(timediff*1000).ceil} #{n[:finish].utc.to_i }"
-              out << "views #{n[:payload][:view_runtime].ceil} #{n[:finish].utc.to_i }"
-              out << "db #{n[:payload][:db_runtime].ceil} #{n[:finish].utc.to_i }"
-              post_data(out.join("\n"))
-            else
-              PLOGGER << "name-#{n[:name]}-"
-            end
-            PLOGGER << "Popped!"
-          end
-          #POST HERE
-          #post_data(out.join("\n"))
-        rescue => e
-          PLOGGER << "Error-#{e.inspect}\n"
-        end
-      end
-    end
-  end
-
   if defined?(PhusionPassenger)
     PhusionPassenger.on_event(:starting_worker_process) do |forked|
       if forked
         Errplane::NotificationsQueue.initialize
-        spawn_thread()
+        Errplane::Instrumentation.spawn_thread()
       end
     end
   else
     Errplane::NotificationsQueue.initialize
-    spawn_thread()
+    Errplane::Instrumentation.spawn_thread()
   end
 end
