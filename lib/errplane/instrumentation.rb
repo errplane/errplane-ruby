126d95ece3e69f62bca086a67bc728e421bb31b3
Todd Persen <todd.persen@gmail.com>
2012-10-04 00:15:32 -0400

    Finish instrumentation cleanup.

--- lib/errplane/instrumentation.rb
+++ lib/errplane/instrumentation.rb
@@ -18,48 +18,46 @@ module Errplane
 
   class Instrumentation
     class << self
+      include Errplane::Logger
+
       def post_data(data)
-        log :info, "Posting data!\n -#{data}"
+        log :info, "Posting data:\n#{data}"
 
         app_key = "app4you2love"
         env_key = "production"
-        log :info, "<<<\n"
-        log :info, data
-        log :info, "<<<\n"
 
         http = Net::HTTP.new("api1.errplane.com", "8086")
         response = http.post("/api/v2/time_series/applications/#{app_key}/environments/#{env_key}?api_key=ignored", data)
-        log :info, "-response.code #{response.code}-\n"
+        log :info, "Response code: #{response.code}"
       end
 
       def spawn_thread()
         Thread.new do
+          log :debug, "Spawning background thread."
           while true
-            sleep 2
+            log :debug, "Checking background queue."
+            sleep 5
             begin
-              out = []
               while !Errplane::NotificationsQueue.notifications.empty?
-                log :info, "not empty!"
-                n = ErrplaneNotifications.notifications.pop
+                log :info, "Found data in the queue."
+                n = Errplane::NotificationsQueue.notifications.pop
                 timediff = n[:finish] - n[:start]
-                log :info, "------------------------\n"
-                s = ["notification2:", n[:name], timediff, n[:nid], n[:payload], "\n"].join("\t")
-                log :info, s
                 if( n[:name].to_s == "process_action.action_controller" )
-                  out << "controllers/#{n[:payload][:controller]}/actions/#{n[:payload][:action]} #{(timediff*1000).ceil} #{n[:finish].utc.to_i}"
-                  out << "controllers/#{n[:payload][:controller]}/actions/#{n[:payload][:action]}/views #{n[:payload][:view_runtime].ceil} #{n[:finish].utc.to_i }"
-                  out << "controllers/#{n[:payload][:controller]}/actions/#{n[:payload][:action]}/db #{n[:payload][:db_runtime].ceil} #{n[:finish].utc.to_i }"
-                  out << "controllers #{(timediff*1000).ceil} #{n[:finish].utc.to_i }"
-                  out << "views #{n[:payload][:view_runtime].ceil} #{n[:finish].utc.to_i }"
-                  out << "db #{n[:payload][:db_runtime].ceil} #{n[:finish].utc.to_i }"
-                  post_data(out.join("\n"))
+                  data = [].tap do |line|
+                    line << "controllers/#{n[:payload][:controller]}/actions/#{n[:payload][:action]} #{(timediff*1000).ceil} #{n[:finish].utc.to_i}"
+                    line << "controllers/#{n[:payload][:controller]}/actions/#{n[:payload][:action]}/views #{n[:payload][:view_runtime].ceil} #{n[:finish].utc.to_i }"
+                    line << "controllers/#{n[:payload][:controller]}/actions/#{n[:payload][:action]}/db #{n[:payload][:db_runtime].ceil} #{n[:finish].utc.to_i }"
+                    line << "controllers #{(timediff*1000).ceil} #{n[:finish].utc.to_i }"
+                    line << "views #{n[:payload][:view_runtime].ceil} #{n[:finish].utc.to_i }"
+                    line << "db #{n[:payload][:db_runtime].ceil} #{n[:finish].utc.to_i }"
+                  end
+                  post_data(data.join("\n"))
                 else
-                  log :info, "name-#{n[:name]}-"
+                  log :info, "Ignored instrumentation: #{n[:name]}"
                 end
-                log :info, "Popped!"
               end
             rescue => e
-              log :info, "Error-#{e.inspect}\n"
+              log :info, "Instrumentation Error! #{e.inspect}"
             end
           end
         end
