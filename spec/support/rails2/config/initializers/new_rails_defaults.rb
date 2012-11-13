c4c426f8e13e1206fc925b174c0395e188bbb686
Todd Persen <todd.persen@gmail.com>
2012-11-13 16:34:12 -0500

    More rails 2.3 cleanup.

--- spec/support/rails2/config/initializers/new_rails_defaults.rb
+++
@@ -1,8 +0,0 @@
-if defined?(ActiveRecord)
-  ActiveRecord::Base.include_root_in_json = true
-  ActiveRecord::Base.store_full_sti_class = true
-end
-
-ActionController::Routing.generate_best_match = false
-ActiveSupport.use_standard_json_time_format = true
-ActiveSupport.escape_html_entities_in_json = false
