9408e797d8b0fb757e0b51c0dca47a29cecc955f
Todd Persen <todd.persen@gmail.com>
2012-12-10 23:36:31 -0500

    Block instrumentation in Rails 2.3.x when it's disabled in the configuration.

--- lib/errplane/rails/benchmarking.rb
+++ lib/errplane/rails/benchmarking.rb
@@ -12,18 +12,24 @@ module Errplane
       private
       def perform_action_with_instrumentation
         ms = Benchmark.ms { perform_action_without_instrumentation }
-        Errplane.report "controllers/#{params[:controller]}/#{params[:action]}", :value => ms.ceil
+        if Errplane.configuration.instrumentation_enabled
+          Errplane.report "controllers/#{params[:controller]}/#{params[:action]}", :value => ms.ceil
+        end
       end
 
       def view_runtime_with_instrumentation
         runtime = view_runtime_without_instrumentation
-        Errplane.report "views", :value => runtime.split.last.to_f.ceil
+        if Errplane.configuration.instrumentation_enabled
+          Errplane.report "views", :value => runtime.split.last.to_f.ceil
+        end
         runtime
       end
 
       def active_record_runtime_with_instrumentation
         runtime = active_record_runtime_without_instrumentation
-        Errplane.report "db", :value => runtime.split.last.to_f.ceil
+        if Errplane.configuration.instrumentation_enabled
+          Errplane.report "db", :value => runtime.split.last.to_f.ceil
+        end
         runtime
       end
     end
