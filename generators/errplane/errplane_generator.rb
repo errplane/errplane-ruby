25ecc6f93cd1a04b091102c83a36b30e6af2d018
Todd Persen <todd.persen@gmail.com>
2013-02-28 12:38:09 -0800

    Fix generator for Rails 2.x

--- generators/errplane/errplane_generator.rb
+++ generators/errplane/errplane_generator.rb
@@ -14,9 +14,13 @@ class ErrplaneGenerator < Rails::Generator::Base
       puts "Contacting Errplane API"
       application_name = "ApplicationName"
       api_key = options[:api_key]
-      http = Net::HTTP.new("app.errplane.com", "80")
+
+      connection = Net::HTTP.new("errplane.com", 443)
+      connection.use_ssl = true
+      connection.verify_mode = OpenSSL::SSL::VERIFY_NONE
       url = "/api/v1/applications?api_key=#{api_key}&name=#{application_name}"
-      response = http.post(url, nil)
+      response = connection.post(url, nil)
+
       @application = JSON.parse(response.body)
 
       unless response.is_a?(Net::HTTPSuccess)
@@ -25,13 +29,14 @@ class ErrplaneGenerator < Rails::Generator::Base
     rescue => e
       puts "We ran into a problem creating your application via the API!"
       puts "If this issue persists, contact us at support@errplane.com with the following details:"
+      puts "API Key: #{e.class}: #{options[:api_key]}"
       puts "#{e.class}: #{e.message}"
     end
 
     record do |m|
       m.template "initializer.rb", "config/initializers/errplane.rb",
         :assigns => {
-          :application_id => options[:application_id] || secure_random.hex(4),
+          :application_id => options[:application_id] || @application["key"] || secure_random.hex(4),
           :api_key => options[:api_key]
         }
     end
