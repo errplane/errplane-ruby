137568a1ce87eac6e238fbcaf9abaebc8b53cd7e
Todd Persen <todd.persen@gmail.com>
2013-03-11 10:25:26 -0700

    Update generator API calls.

--- lib/rails/generators/errplane/errplane_generator.rb
+++ lib/rails/generators/errplane/errplane_generator.rb
@@ -4,21 +4,28 @@ class ErrplaneGenerator < Rails::Generators::Base
   desc "Description:\n  This creates a Rails initializer for Errplane."
 
   begin
-    puts "Contacting Errplane API"
-    application_name = Rails.application.class.parent_name || "NewApplication"
-    api_key = ARGV.last
+    if ARGV.count == 1
+      puts "No Application ID provided, contacting Errplane API."
+      application_name = Rails.application.class.parent_name || "NewApplication"
+      api_key = ARGV.first
 
-    connection = Net::HTTP.new("errplane.com", 443)
-    connection.use_ssl = true
-    connection.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    url = "/api/v1/applications?api_key=#{api_key}&name=#{application_name}"
-    response = connection.post(url, nil)
+      connection = Net::HTTP.new("errplane.com", 443)
+      connection.use_ssl = true
+      connection.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      url = "/api/v1/applications?api_key=#{api_key}&name=#{application_name}"
+      response = connection.post(url, nil)
 
-    @application = JSON.parse(response.body)
+      unless response.is_a?(Net::HTTPSuccess)
+        raise "The Errplane API returned an error: #{response.inspect}"
+      end
 
-    unless response.is_a?(Net::HTTPSuccess)
-      raise "The Errplane API returned an error: #{response.inspect}"
+      @application = JSON.parse(response.body)
+      @application_id = @application["key"]
+    else
+      @application_id = ARGV[1]
     end
+    puts "Received Application ID: #{@application_id}"
+
   rescue => e
     puts "We ran into a problem creating your application via the API!"
     puts "If this issue persists, contact us at support@errplane.com with the following details:"
@@ -32,7 +39,7 @@ class ErrplaneGenerator < Rails::Generators::Base
     :description => "API key for your Errplane organization"
   argument :application_id,
     :required => false,
-    :default => @application["key"],
+    :default => @application_id,
     :type => :string,
     :description => "Identifier for this application (Leave blank and a new one will be generated for you)"
 
